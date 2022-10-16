2c37a435a76ec3b731f4305a24feed9d40ef0f31
Abhi <abhirup@rhymetech.io>
2022-10-16 13:29:38 +0530

    Added common functions

---
+++ token-project/src/utility/shyft.js
@@ -0,0 +1,32 @@
+import {clusterApiUrl, Connection, Keypair, Transaction } from '@solana/web3.js';
+import { NodeWallet } from '@metaplex/js';
+import { decode } from 'bs58';
+import { Buffer } from 'buffer';
+
+//import { ShyftWallet } from '../types';
+
+export async function confirmTransactionFromBackend(network, encodedTransaction, privateKey) {
+  const connection = new Connection(clusterApiUrl(network), 'confirmed');
+  const feePayer = Keypair.fromSecretKey(decode(privateKey));
+  const wallet = new NodeWallet(feePayer);
+  const recoveredTransaction = Transaction.from(
+    Buffer.from(encodedTransaction, 'base64')
+  );
+  const signedTx = await wallet.signTransaction(recoveredTransaction);
+  const confirmTransaction = await connection.sendRawTransaction(
+    signedTx.serialize()
+  );
+  return confirmTransaction;
+}
+
+export async function confirmTransactionFromFrontend(connection, encodedTransaction, wallet) {
+  console.log(encodedTransaction);
+  const recoveredTransaction = Transaction.from(
+    Buffer.from(encodedTransaction, 'base64')
+  );
+  const signedTx = await wallet.signTransaction(recoveredTransaction);
+  const confirmTransaction = await connection.sendRawTransaction(
+    signedTx.serialize()
+  );
+  return confirmTransaction;
+}
