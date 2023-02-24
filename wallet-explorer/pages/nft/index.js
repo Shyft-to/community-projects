3d1bdb9e63738263935b7b2ef431064bf5a33187
Abhi <abhirup@rhymetech.io>
2023-02-24 20:24:39 +0530

    Added nft module and token design

---
+++ wallet-explorer/pages/nft/index.js
@@ -0,0 +1,13 @@
+import NftExpanded from "@/components/NftExpanded";
+
+const nft = () => {
+    return ( 
+        <div className="background">
+            <div className="container pt-5">
+                <NftExpanded />
+            </div>
+        </div>
+     );
+}
+ 
+export default nft;
