8a57652e5d003a2b9438ad4448fec6369b727efa
Abhirup <abhirup@rhymetech.io>
2023-03-12 03:08:23 +0530

    Added search results to home

--- wallet-explorer/src/Home.js
+++ wallet-explorer/src/Home.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import Typewriter from 'typewriter-effect';
@@ -13,16 +14,66 @@ import styles from "./resources/css/Home.module.css";
 import searchIcon from "./resources/images/uil_search.svg";
 
 const Home = () => {
+  const navigate = useNavigate();
   const [wallet, setWallet] = useState('');
   const [network, setNetwork] = useState('mainnet-beta')
+
+  const [isFocused, setFocused] = useState(false);
+
+  const [searchData, setSearchData] = useState([]);
+
+  useEffect(() => {
+    try {
+      const searchHistory = JSON.parse(localStorage.getItem("shshis") || "[]");
+
+      if (Array.isArray(searchHistory) && searchHistory.length > 0) {
+        setSearchData(searchHistory);
+      }
+    } catch (error) {
+      setSearchData([]);
+    }
+
+
+  }, [])
+
+  const BlurAfterTime = () => {
+    setTimeout(() => {
+      setFocused(false)
+    }, 200);
+  }
+
+
+  const addDataNavigate = (wallet, network) => {
+    console.log("clicked");
+    try {
+      if (wallet !== "") {
+        const newAddress = {
+          address: wallet,
+          network: network
+        }
+        var newResults = [];
+        if (searchData.length > 4)
+          newResults = [...searchData.slice(1), newAddress];
+        else
+          newResults = [...searchData, newAddress];
+
+        setSearchData(newResults);
+        localStorage.setItem('shshis', JSON.stringify(newResults));
+        navigate(`/address/${wallet}?cluster=${network}`);
+
+      }
+    } catch (error) {
+      navigate(`/address/${wallet}?cluster=${network}`);
+    }
+
+  }
   return (
     <div>
       <div className={styles.background_2}>
-     
         <div className="container-lg">
           <div className={styles.central_area}>
             <div className={styles.main_title_container}>
-              <motion.div initial={{ opacity: 0, y:30 }} animate={{ opacity: 1, y:0 }} transition={{delay:0.4}}>Solana Translator</motion.div>
+              <motion.div initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.4 }}>Solana Translator</motion.div>
 
               {/* <div className="d-flex flex-wrap justify-content-center">
                 <motion.div initial={{ opacity: 0, y:30 }} animate={{ opacity: 1, y:0 }} transition={{delay:0.4}}>Solana Translator</motion.div>
@@ -43,19 +94,32 @@ const Home = () => {
                   />
                 </div>
               </div> */}
+
             </div>
-            <motion.div className="row py-5" initial={{ opacity: 0, y:30 }} animate={{ opacity: 1, y:0 }} transition={{delay:0.6}}>
+            <motion.div className="row py-5" initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.6 }}>
               <div className="col-12 col-md-8 p-2">
                 <div className={styles.simple_input_container}>
                   <div className="d-flex">
                     <div className="flex-grow-1">
-                      <input type="text" placeholder="Explore Solana" value={wallet} onChange={(e) => setWallet(e.target.value)} />
+                      <input type="text" placeholder="Explore Solana" value={wallet} onChange={(e) => setWallet(e.target.value)} onFocus={() => setFocused(true)} onBlur={BlurAfterTime} />
                     </div>
                     <div style={{ marginTop: "-1px", color: "#fff" }}>
                       <img src={searchIcon} alt="Search Box" />
                     </div>
                   </div>
-
+                  {isFocused && <div className={styles.search_area}>
+                    {searchData.map((result) => (<motion.button className={styles.each_item} whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.98 }}>
+                      <div className="d-flex">
+                        <div className={`flex-grow-1 ${styles.address_area}`}>
+                          {result.address}
+                        </div>
+                        <div className={styles.network_area}>
+                        {(result.network === "mainnet-beta")?<span className="text-light">mainnet</span>:(result.network === "testnet")?<span className="text-warning">testnet</span>:<span className="text-info">devnet</span>}
+                        </div>
+                      </div>
+                    </motion.button>))}
+                    
+                  </div>}
                 </div>
               </div>
               <div className="col-12 col-md-4 p-2">
@@ -69,7 +133,7 @@ const Home = () => {
               </div>
             </motion.div>
             <div className="text-center">
-              <motion.div className={styles.button_container} initial={{ opacity: 0, y:30 }} animate={{ opacity: 1, y:0 }} transition={{delay:0.9}}>
+              <motion.div className={styles.button_container} initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.9 }}>
                 <Link to={`/address/${wallet}?cluster=${network}`} className={styles.btn_solid_grad}>
                   Translate
                 </Link>
--- wallet-explorer/src/components/SearchComponent.js
+++ wallet-explorer/src/components/SearchComponent.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 // import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { FaSearch } from "react-icons/fa";
 import { motion } from "framer-motion";
@@ -18,14 +18,14 @@ const SearchComponent = () => {
   useEffect(() => {
     try {
       const searchHistory = JSON.parse(localStorage.getItem("shshis") || "[]");
-    
+
       if (Array.isArray(searchHistory) && searchHistory.length > 0) {
         setSearchData(searchHistory);
       }
     } catch (error) {
       setSearchData([]);
     }
-    
+
 
   }, [])
 
@@ -36,21 +36,20 @@ const SearchComponent = () => {
   }
 
 
-  const addDataNavigate = (wallet,network) => {
+  const addDataNavigate = (wallet, network) => {
     console.log("clicked");
     try {
-      if(wallet !== "")
-      {
+      if (wallet !== "") {
         const newAddress = {
           address: wallet,
           network: network
         }
         var newResults = [];
-        if(searchData.length>4)
-          newResults = [...searchData.slice(1),newAddress];
+        if (searchData.length > 4)
+          newResults = [...searchData.slice(1), newAddress];
         else
-          newResults = [...searchData,newAddress];
-        
+          newResults = [...searchData, newAddress];
+
         setSearchData(newResults);
         localStorage.setItem('shshis', JSON.stringify(newResults));
         navigate(`/address/${wallet}?cluster=${network}`);
@@ -59,75 +58,86 @@ const SearchComponent = () => {
     } catch (error) {
       navigate(`/address/${wallet}?cluster=${network}`);
     }
-    
+
   }
   return (
     <div>
-      <motion.div className={styles.form_container} initial={{ opacity: 0, y: -100 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }}>
-        <div className={styles.search_n_suggestions} >
-          <div className={styles.form_field_outer}>
-            <div className={styles.form_field_inner}>
-              <div className="d-flex justify-content-start">
+      <div className="row">
+        <div className="col-12 col-lg-2">
+          <div className="logo_container">
+            <Link to={`/`}>Solana Explorer</Link>
+          </div>
+        </div>
+        <div className="col-12 col-lg-8">
+          <motion.div className={styles.form_container} initial={{ opacity: 0, y: -100 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }}>
+            <div className={styles.search_n_suggestions} >
+              <div className={styles.form_field_outer}>
+                <div className={styles.form_field_inner}>
+                  <div className="d-flex justify-content-start">
 
-                <select
-                  className="me-4"
-                  value={network}
-                  onChange={(e) => setNetwork(e.target.value)}
-                >
-                  <option value="mainnet-beta">Mainnet</option>
-                  <option value="devnet">Devnet</option>
-                  <option value="testnet">Testnet</option>
-                </select>
-                <div className="flex-grow-1">
-
-                  <div className="d-flex justify-content-between">
+                    <select
+                      className="me-4"
+                      value={network}
+                      onChange={(e) => setNetwork(e.target.value)}
+                    >
+                      <option value="mainnet-beta">Mainnet</option>
+                      <option value="devnet">Devnet</option>
+                      <option value="testnet">Testnet</option>
+                    </select>
                     <div className="flex-grow-1">
-                      <input
-                        type="text"
-                        placeholder="Explore Solana"
-                        value={wallet}
-                        onChange={(e) => setWallet(e.target.value)}
-                        onFocus={() => setFocused(true)} 
-                        onBlur={BlurAfterTime}
-                      />
-                    </div>
-                    <div>
-                      {/* <Link to={`/address/${wallet}?cluster=${network}`} className={styles.search_icon}>
+
+                      <div className="d-flex justify-content-between">
+                        <div className="flex-grow-1">
+                          <input
+                            type="text"
+                            placeholder="Explore Solana"
+                            value={wallet}
+                            onChange={(e) => setWallet(e.target.value)}
+                            onFocus={() => setFocused(true)}
+                            onBlur={BlurAfterTime}
+                          />
+                        </div>
+                        <div>
+                          {/* <Link to={`/address/${wallet}?cluster=${network}`} className={styles.search_icon}>
                                 <FaSearch />
                             </Link> */}
-                      <button onClick={() => addDataNavigate(wallet,network)} style={{ backgroundColor: "transparent", border: "none", outline: "none" }} className={styles.search_icon}>
-                        <FaSearch />
-                      </button>
+                          <button onClick={() => addDataNavigate(wallet, network)} style={{ backgroundColor: "transparent", border: "none", outline: "none" }} className={styles.search_icon}>
+                            <FaSearch />
+                          </button>
+                        </div>
+
+                      </div>
+
                     </div>
 
                   </div>
-
                 </div>
-
               </div>
-            </div>
-          </div>
-          
-          {isFocused && <div className={styles.suggestions_area_outer}>
-          
-            {<motion.div className={styles.suggestions_area} initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
-              {(searchData.length > 0) && (searchData.map((result) => (<button className={styles.each_search} onClick={() => addDataNavigate(result.address,result.network)}>
-                <div className="d-flex">
-                  <div className={styles.network_area}>
-                    {(result.network === "mainnet-beta")?<span className="text-light">mainnet</span>:(result.network === "testnet")?<span className="text-warning">testnet</span>:<span className="text-info">devnet</span>}
-                  </div>
-                  <div className={`flex-grow-1 ${styles.address_area}`}>
-                    {result.address}
-                  </div>
-                </div>
-              </button>)
-             ))}
-             </motion.div>  
-            }
 
-          </div>}
+              {isFocused && <div className={styles.suggestions_area_outer}>
+
+                {<motion.div className={styles.suggestions_area} initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+                  {(searchData.length > 0) && (searchData.map((result) => (<motion.button className={styles.each_search} onClick={() => addDataNavigate(result.address, result.network)} whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.98 }}>
+                    <div className="d-flex">
+                      <div className={styles.network_area}>
+                        {(result.network === "mainnet-beta") ? <span className="text-light">mainnet</span> : (result.network === "testnet") ? <span className="text-warning">testnet</span> : <span className="text-info">devnet</span>}
+                      </div>
+                      <div className={`flex-grow-1 ${styles.address_area}`}>
+                        {result.address}
+                      </div>
+                    </div>
+                  </motion.button>)
+                  ))}
+                </motion.div>
+                }
+
+              </div>}
+            </div>
+          </motion.div>
         </div>
-      </motion.div>
+        <div className="col-12 col-lg-2"></div>
+      </div>
+
     </div>
   );
 };
